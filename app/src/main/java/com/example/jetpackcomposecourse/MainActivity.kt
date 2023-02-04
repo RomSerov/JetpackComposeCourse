f81f22569e2af7b1689b8b35762c800a24ec4fd1
grey- <grey-552@yandex.ru>
2023-02-04 17:51:01 +0300

    VKAuthActivityResult

--- app/src/main/java/com/example/jetpackcomposecourse/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecourse/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.jetpackcomposecourse
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import com.example.jetpackcomposecourse.ui.theme.JetpackComposeCourseTheme
 import com.example.jetpackcomposecourse.ui.screens.main.MainScreen
+import com.example.jetpackcomposecourse.ui.theme.JetpackComposeCourseTheme
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 
 class MainActivity : ComponentActivity() {
 
@@ -12,6 +16,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeCourseTheme {
+
+                // авторизация VK, подписка на результат
+                val launcher = rememberLauncherForActivityResult(
+                    contract = VK.getVKAuthActivityResultContract(),
+                    onResult = {
+                        when (it) {
+                            is VKAuthenticationResult.Failed -> TODO()
+                            is VKAuthenticationResult.Success -> TODO()
+                        }
+                    }
+                )
+
+                launcher.launch(listOf(VKScope.WALL))
+
                 MainScreen()
             }
         }
